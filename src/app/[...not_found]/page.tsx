4ea640f3ef6e0bb47c4cc12e06b208b1944a1bf2
Bryant Richards <dragonseyeimages@gmail.com>
2025-08-13 08:48:45 -0500

    updates 404 page

--- src/app/[...not_found]/page.tsx
+++ src/app/[...not_found]/page.tsx
@@ -1,31 +1,32 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import errorImg from "../../../public/assets/images/error/error-img-3.png";
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import errorImg from '../../../public/assets/images/error/error-img-3.png'
 
 const page = () => {
   return (
     <>
       {/* -- error area start -- */}
-      <div className="container-xxl">
-        <div className="error-wrapper">
-          <div className="error-thumb">
+      <div className='container-xxl'>
+        <div className='error-wrapper'>
+          <div className='error-thumb'>
             <Image
               src={errorImg}
               priority
-              style={{ width: "100%", height: "auto" }}
-              alt="image"
+              style={{ width: '100%', height: 'auto' }}
+              alt='image'
             />
           </div>
-          <div className="error-content">
-            <h2>Oops! This Page is Not Found.</h2>
+          <div className='error-content'>
+            <h2>Hold up!</h2>
             <p>
               The page you are trying to reach is currently unavailable. It may
-              have been moved or no longer exists.
+              have been moved, no longer exists or you may have mistyped the
+              URL.
             </p>
-            <div className="error-btn">
-              <Link href="/" className="btn btn-primary rounded-pill xxl">
-                Back to Home
+            <div className='error-btn'>
+              <Link href='/' className='btn btn-primary rounded-pill xxl'>
+                Return Home
               </Link>
             </div>
           </div>
@@ -33,7 +34,7 @@ const page = () => {
       </div>
       {/* -- error area end -- */}
     </>
-  );
-};
+  )
+}
 
-export default page;
+export default page
